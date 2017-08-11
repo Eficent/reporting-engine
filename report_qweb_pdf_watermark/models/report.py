aa0d9272025e5754cce4b3dab8928fb91d7111bc
aheficent <ahenriquez@eficent.com>
2017-08-11 09:16:06 +0200

    [FIX]suspicious

--- report_qweb_pdf_watermark/models/report.py
+++ report_qweb_pdf_watermark/models/report.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from base64 import b64decode
 from logging import getLogger
-from pyPdf.pdf import PdfFileWriter, PdfFileReader
+from pyPdf import PdfFileWriter, PdfFileReader
 from pyPdf.utils import PdfReadError
 from PIL import Image
 try:
